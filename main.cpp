9efcda55ea85d0ac5b1e90e36bfefa94a732909b
Hayden Reich <hayden.reich@colorado.edu>
2015-05-03 12:48:02 -0600

    Added error checking to initial input request in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,34 @@ using namespace std;
 
 int primeMax()
 {
-    string prime;
+    int prime;
+    bool primeSet = false;
     cout<<"Find all primes up to what number?"<<endl;
     cin>>prime;
-
-    return atoi(prime.c_str());
+    while(primeSet == false){
+        if(!cin){
+            cout<<"Please enter an integer between 1 and 15000"<<endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        else if(prime >= 15000){
+            cout<<"Number too large"<<endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        else if(prime < 1){
+            cout<<"Number too small"<<endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        else{
+            primeSet = true;
+            break;
+        }
+        cout<<"Find all primes up to what number?"<<endl;
+        cin>>prime;
+    }
+    return prime;
 }
 
 
